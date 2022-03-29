d46209b95f4215497b6933b935cb76f13e89ce00
Amaia <amaiacoimbra@gmail.com>
2022-03-28 23:18:40 -0300

    function imc

--- imc.js
+++ imc.js
@@ -5,9 +5,9 @@ const calcular = document.getElementById('calcular')
 
 function imc () {
    
-    const nome = document.getElementById('nome')
-    const altura = document.getElementById('altura')
-    const peso = document.getElementById('peso')
+    const nome = document.getElementById('nome').value
+    const altura = document.getElementById('altura').value
+    const peso = document.getElementById('peso').value
     const resultado = document.getElementById('resultado')
 
     if (nome.value !== '') {
