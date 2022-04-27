01fc68fcbb0c0f7458c8a24239886df18b1e1cbd
Amaia <amaiacoimbra@gmail.com>
2022-04-26 21:46:44 -0300

    att

--- imc.js
+++ imc.js
@@ -17,13 +17,13 @@ function imc () {
         if (valorIMC < 18.5){
             classificacao = 'abaixo do peso.';
         }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
+            classificacao = 'com o peso ideal. Parabéns!!!';
         }else if (valorIMC < 30){
             classificacao = 'levemente acima do peso.';
         }else if (valorIMC < 35){
             classificacao = 'com obesidade grau I.';
         }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II';
+            classificacao = 'com obesidade grau II. Atenção!';
         }else {
             classificacao = 'com obesidade grau III. Cuidado!!';
         }
